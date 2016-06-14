582729a651564bc219d40fe76a7d98895baefd1c
theRingleman <sam.ringleman@gmail.com>
2016-06-13 20:54:21 -0600

    add new list functionality

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,15 @@
 class ListsController < ApplicationController
 
   def index
+    @list = List.new
     @lists = List.all
+  end
+
+  def create
+    @list = List.new
+    @list.name = params[:list][:name]
+    @list.save
+    redirect_to list_path(@list)
   end
 
   def show
