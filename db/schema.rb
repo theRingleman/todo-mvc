2e7fc8e08536c95b1bf58a8a369d9693c775155a
theRingleman <sam.ringleman@gmail.com>
2016-06-29 16:39:49 -0600

    add item schema and show each item a list has in its show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614010848) do
+ActiveRecord::Schema.define(version: 20160629221837) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "list_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
