11c92e204550ce0a0d3a514108f9401395892bb9
theRingleman <sam.ringleman@gmail.com>
2016-06-29 17:04:33 -0600

    Add new item form and functionality

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,14 @@
+class ItemsController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.build(item_params)
+    @item.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:description)
+  end
+end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @item = @list.items.build
   end
 
   private
