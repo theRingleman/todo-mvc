c5ad05e3f990fdadfec5cd924f6b007fbcf36bd9
theRingleman <sam.ringleman@gmail.com>
2016-06-29 21:05:51 -0600

    add status to items, and add #complete? and #incomplete? to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ActiveRecord::Base
   belongs_to :list
   validates :description, presence: true
+
+  STATUS = {
+    incomplete: 0,
+    complete: 1
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
 end
